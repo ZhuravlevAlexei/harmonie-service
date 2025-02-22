25ed14ef990c02a55323055668f19cc095dbda7b
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2025-02-22 17:56:12 +0200

    clean http client

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { env } from './env';
 
 const httpClient = axios.create({
-  // baseURL: 'https://api.example.com', // Базовый URL
   baseURL: 'https://my.prom.ua/api/v1/',
   //   timeout: 5000, // Таймаут 5 секунд
   headers: {
