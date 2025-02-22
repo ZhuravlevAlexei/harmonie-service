35f497797762c5859a21587e07b7bbb21908e07d
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2025-02-22 14:34:47 +0200

    just save XML

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
+import { env } from './env';
 
 const httpClient = axios.create({
-  // baseURL: 'https://api.example.com', // Базовый URL
   baseURL: 'https://my.prom.ua/api/v1/',
   //   timeout: 5000, // Таймаут 5 секунд
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.API_TOKEN}`,
+    Authorization: `Bearer ${env({ name: 'PROM_API_TOKEN' })}`,
   },
 });
 
