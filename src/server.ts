d4df9f6edbf622120300000f91a8eaeca0c49a22
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2025-06-23 17:18:40 +0300

    comment https variant for deploy on render.com

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,29 @@
 import express from 'express';
-import https from 'https';
+
 import cors from 'cors';
 import pino from 'pino-http';
-import fs from 'fs';
-import path from 'node:path';
 
 import router from 'routers';
 import { notFoundHandler } from './middlewares/notFoundHandler';
 import { errorHandler } from './middlewares/errorHandler';
 import { env } from './utils/env';
-import { SERTIFICATES_DIR } from './constants/index';
+
+//эти пути к сертификатам не нужны для деплоя на render.com
+// но они нужны для локального тестирования https варианта сервера
+// import path from 'node:path';
+// import { SERTIFICATES_DIR } from './constants/index';
+// import fs from 'fs';
+// import https from 'https';
+
+// const PATH_TO_SERT_KEY = path.join(SERTIFICATES_DIR, 'server.key');
+// const PATH_TO_SERT_CERT = path.join(SERTIFICATES_DIR, 'server.crt');
+// const options = {
+//   key: fs.readFileSync(PATH_TO_SERT_KEY),
+//   cert: fs.readFileSync(PATH_TO_SERT_CERT),
+// };
+//конец для локального тестирования https-сервера
 
 const PORT = Number(env('PORT', '3000'));
-const PATH_TO_SERT_KEY = path.join(SERTIFICATES_DIR, 'server.key');
-const PATH_TO_SERT_CERT = path.join(SERTIFICATES_DIR, 'server.crt');
-const options = {
-  key: fs.readFileSync(PATH_TO_SERT_KEY),
-  cert: fs.readFileSync(PATH_TO_SERT_CERT),
-};
 
 export const setupServer = () => {
   const app = express();
@@ -37,8 +43,12 @@ export const setupServer = () => {
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
-  // https  server
-  https.createServer(options, app).listen(PORT, () => {
+  // // https  server толко для локального тестирования
+  // https.createServer(options, app).listen(PORT, () => {
+  //   console.log(`Server is running on port ${PORT}`);
+  // });
+
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
